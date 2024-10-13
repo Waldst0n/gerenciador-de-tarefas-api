f3b56d4919f489c9306c228251dd85254075e540
Waldston <waldston.ws@gmail.com>
2024-10-13 13:22:57 -0300

    refactor: add cors and port alterations

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 const connectToDatabase = require('./src/database/mongoose.database');
 const TaskRouter = require('./src/routes/task.routes');
 
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 connectToDatabase();
 
 app.use('/tasks', TaskRouter);
 
-app.listen(3000, () => {
+app.listen(8000, () => {
     console.log('Servidor Online');
 });
