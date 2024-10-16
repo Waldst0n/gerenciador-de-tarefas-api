31fe524565ff8ddb98708fe844089bae89fcf4f6
Waldston <waldston.ws@gmail.com>
2024-10-16 19:19:20 -0300

    refact: script start

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ connectToDatabase();
 
 app.use('/tasks', TaskRouter);
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`); // Mensagem de confirmação
+app.listen({
+    host: '0.0.0.0',
+    port: process.env.PORT ? Number(process.env.PORT) : 8000,
+}).then(() => {
+    console.log('Http Server Running');
 });
