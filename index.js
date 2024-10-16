f68508de2f389365fcca6db32facc0c680225def
Waldston <waldston.ws@gmail.com>
2024-10-16 11:47:50 -0300

    update Port

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ const TaskRouter = require('./src/routes/task.routes');
 dotenv.config();
 const app = express();
 
-const PORT = 8000;
-
+const PORT = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 
